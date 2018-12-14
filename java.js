8735adaf616e5af7d8df5d15bf3a3c8110e68393
yvetteveve <yvetteumubyeyi2@gmail.com>
2018-12-14 14:52:12 +0200

    i will styling in css

--- java.js
+++ java.js
@@ -1,3 +1,47 @@
-if (side1===side2&&side2===side3){
-    alert()
+
+function outputname(){
+    var numberone=parseInt(document.getElementById('one').value);
+    var numbertwo=parseInt(document.getElementById('two').value);
+    var numberthree=parseInt(document.getElementById('three').value);
+    var array=[numberone,numbertwo,numberthree];
+    var text;
+
+    if(numberone <= 0|| numbertwo <= 0 || numberthree <=0 ){
+       text="Undefined"
+    }
+    else if (
+        (numberone + numbertwo <= numberthree &&
+            numbertwo + numberone <= numberthree)||
+            (numberone + numberthree <= numbertwo &&
+             numberthree + numberone <= numbertwo)||
+            (numbertwo + numberthree <= numberone && numberthree + numbertwo <= numberone)
+         ) {
+        text = "Not a Triangle.";
+      }
+    
+    else if(numberone === numbertwo && 
+        numberone===numberthree &&
+        numbertwo === numberthree){
+        text = "Equilaterial Triangle.";
+    }
+     
+    
+    else if(numberone===numbertwo || 
+        numberone===numberthree || 
+        numbertwo===numberthree) {
+        text="Isosceles Triangle.";
+    }
+  
+    else if(numberone!==numbertwo &&
+         numberone!==numberthree &&
+         numbertwo!==numberthree){
+         text="Scalene Triangle."
+    }
+   
+        else{
+        text = "Error.";
+    }
+    
+        document.getElementById('demo').innerHTML= text;
+
 }
